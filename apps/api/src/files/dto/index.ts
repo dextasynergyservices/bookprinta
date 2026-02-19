c329d116b9c56ac88ace9c4bf44672cd7c1b1bd5
eyurenalison <eeyuren1@gmail.com>
2026-02-19 02:48:23 +0100

    feature: Set up Cloudinary signed upload infrastructure

---
+++ apps/api/src/files/dto/index.ts
@@ -0,0 +1,7 @@
+export type { ConfirmUploadInput, GenerateSignatureInput } from "./files.dto.js";
+export {
+  ConfirmUploadDto,
+  ConfirmUploadSchema,
+  GenerateSignatureDto,
+  GenerateSignatureSchema,
+} from "./files.dto.js";
