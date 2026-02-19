824aca00fd62a2a0cef5f96d92af0a7507fe9684
Edirin Alison Eyuren <36126320+eyurenalison@users.noreply.github.com>
2026-02-19 04:26:57 +0100

    Feature/#16 setup bullmq with redis (#66)
    * ci: added github actions (#53) (#54)
    
    * ci: added github actions
    
    * fix: CI error, prisma to run in lint job
    
    * fix: CI error, db url error
    
    * refactor: prisma.config.ts file
    
    * Develop (#56)
    
    * ci: added github actions (#53)
    
    * ci: added github actions
    
    * fix: CI error, prisma to run in lint job
    
    * fix: CI error, db url error
    
    * refactor: prisma.config.ts file
    
    * feat: implemented jst auth, login, login, register apis (#55)
    
    ---------
    
    Co-authored-by: Edirin Alison Eyuren <36126320+eyurenalison@users.noreply.github.com>
    
    * Merge develop to main (#59)
    
    * ci: added github actions (#53)
    
    * ci: added github actions
    
    * fix: CI error, prisma to run in lint job
    
    * fix: CI error, db url error
    
    * refactor: prisma.config.ts file
    
    * feat: implemented jst auth, login, login, register apis (#55)
    
    * festure: implemented RABC guards (#57)
    
    ---------
    
    Co-authored-by: Dexta Synergy Services <147712845+dextasynergyservices@users.noreply.github.com>
    
    * Develop (#60)
    
    * ci: added github actions (#53)
    
    * ci: added github actions
    
    * fix: CI error, prisma to run in lint job
    
    * fix: CI error, db url error
    
    * refactor: prisma.config.ts file
    
    * feat: implemented jst auth, login, login, register apis (#55)
    
    * festure: implemented RABC guards (#57)
    
    ---------
    
    Co-authored-by: Edirin Alison Eyuren <36126320+eyurenalison@users.noreply.github.com>
    
    * Merge develop to main 2 (#63)
    
    * ci: added github actions (#53)
    
    * ci: added github actions
    
    * fix: CI error, prisma to run in lint job
    
    * fix: CI error, db url error
    
    * refactor: prisma.config.ts file
    
    * feat: implemented jst auth, login, login, register apis (#55)
    
    * festure: implemented RABC guards (#57)
    
    * feature: Set up Cloudinary signed upload infrastructure (#61)
    
    ---------
    
    Co-authored-by: Dexta Synergy Services <147712845+dextasynergyservices@users.noreply.github.com>
    
    * Merge develop to main 3 (#65)
    
    * ci: added github actions (#53)
    
    * ci: added github actions
    
    * fix: CI error, prisma to run in lint job
    
    * fix: CI error, db url error
    
    * refactor: prisma.config.ts file
    
    * feat: implemented jst auth, login, login, register apis (#55)
    
    * festure: implemented RABC guards (#57)
    
    * feature: Set up Cloudinary signed upload infrastructure (#61)
    
    * feature: Set up Upstash Redis connection (#64)
    
    ---------
    
    Co-authored-by: Dexta Synergy Services <147712845+dextasynergyservices@users.noreply.github.com>
    
    * feature: Set up BullMQ with Redis
    
    ---------
    
    Co-authored-by: Dexta Synergy Services <147712845+dextasynergyservices@users.noreply.github.com>

---
+++ apps/api/src/jobs/index.ts
@@ -0,0 +1,8 @@
+export {
+  ALL_QUEUES,
+  JOB_NAMES,
+  QUEUE_AI_FORMATTING,
+  QUEUE_PAGE_COUNT,
+  QUEUE_PDF_GENERATION,
+} from "./jobs.constants.js";
+export { JobsModule } from "./jobs.module.js";
