d96b12fec5d16e80942082381f34929329d660c7
ROWLAND LAWSON <lawson.o.rowland@gmail.com>
2026-02-21 00:56:23 +0100

    feat: add navbar and footer (#107)

--- apps/web/app/[locale]/(marketing)/layout.tsx
+++ apps/web/app/[locale]/(marketing)/layout.tsx
@@ -1,9 +1,12 @@
+import { Footer } from "@/components/marketing/footer";
+import { Navbar } from "@/components/marketing/navbar";
+
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* TODO: Navigation component */}
-      <main>{children}</main>
-      {/* TODO: Footer component */}
+      <Navbar />
+      <main id="main-content">{children}</main>
+      <Footer />
     </>
   );
 }
